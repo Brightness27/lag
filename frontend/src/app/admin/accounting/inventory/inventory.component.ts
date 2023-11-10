12520d7771c3066d0d27c4c66a65dfecc5705a16
ivan <ivan.caliwanagan@cvsu.edu.ph>
2023-11-10 15:48:33 +0800

    new commit

--- frontend/src/app/admin/accounting/inventory/inventory.component.ts
+++ frontend/src/app/admin/accounting/inventory/inventory.component.ts
@@ -16,16 +16,63 @@ export class InventoryComponent implements OnInit {
   isSideNavCollapsed = false;
   screenWidth = 0;
 
+  department!: any;
+
+  name: string =  '';
+
+  superadmin = false;
+  hr = false;
+  accounting = false;
+  engineering = false;
+
   token: any;
 
   constructor(private adminService: AdminServicesService) {}
 
   ngOnInit(): void {
     this.token = this.adminService.getTokenDetails();
+
+    if(this.token) {
+      const id = this.token.adminId;
+      this.adminService.getAdminById(id).subscribe(admin => {
+        this.department = admin.department;
+        this.name = 'Hello, ' + admin.fname;
+        this.setActiveDepartment();
+      });
+    }
+  }
+
+  setActiveDepartment() {
+    if(this.department == 'general admin') {
+      this.superadmin = true;
+      
+    }
+    else if(this.department == 'hr') {
+      this.hr = true;
+    }
+    else if(this.department == 'accounting') {
+      this.accounting = true;
+    }
+    else if(this.department == 'engineering') {
+      this.engineering = true;
+    }
+    
   }
 
   onToggleSideNav(data: SideNavToggle): void {
     this.screenWidth = data.screenWidth;
     this.isSideNavCollapsed = data.collapsed;
+  }
+
+  getBodyClass(): string {
+    let styleClass = '';
+    if(this.isSideNavCollapsed && this.screenWidth > 768) {
+      styleClass = 'body-trimmed';
+    }
+    else if(this.isSideNavCollapsed && this.screenWidth <= 768 && this.screenWidth > 0) {
+      styleClass = 'body-md-screen';
+    }
+
+    return styleClass;
   }
 }
